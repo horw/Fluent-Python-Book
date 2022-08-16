5ad405872db3087dfe3eabb626f7a510b144d0cb
Horw <horw7001@gmail.com>
2022-08-16 11:37:35 +0300

    chapter 5

---
+++ example5-10.py
@@ -0,0 +1,23 @@
+def tag(name, *content, cls=None, **attrs):
+
+    if cls is not None:
+        attrs['class'] = cls
+
+    if attrs:
+        attr_str = ''.join(' %s="%s"' % (attr, value)
+                           for attr,value in sorted(attrs.items()))
+
+    else:
+        attr_str = ''
+
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' %
+                         (name, attr_str, c, name) for c in content
+                         )
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+print(
+    tag('br'),'\n',
+    tag(name='test',content = ('hello','world'), bl='w')
+)
